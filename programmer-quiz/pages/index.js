46c08e1b689acaab28a8fbc542b1cdf3397463d3
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-01-26 19:56:15 -0300

    Updated pages

--- programmer-quiz/pages/index.js
+++ programmer-quiz/pages/index.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import cf from '../cf.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+// eslint-disable-next-line import/no-named-as-default
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${ cf.bg });
@@ -25,16 +29,41 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
-    <QuizBackground backgroundImage={ cf.bg }>
+    <QuizBackground backgroundImage={cf.bg}>
+      <Head>
+        <title>Quiz-Programming</title>
+      </Head>
+
       <QuizContainer>
         <QuizLogo />
+
         <Widget>
           <Widget.Header>
             <h1>{ cf.title }</h1>
           </Widget.Header>
+
           <Widget.Content>
             <p>{ cf.description }</p>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (event) {
+                  setName(event.target.value);
+                }}
+                placeholder="Qual seu nome?"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Play
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
